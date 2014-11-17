40f7e67649edbe5e139f05a0c56087ff96b3de34
dazeycm <dazeycm@miamioh.edu>
2014-11-17 11:55:16 -0500

    Changed methods to use referencing

--- Graph.cpp
+++ Graph.cpp
@@ -5,8 +5,6 @@
 */
 #include "Graph.h"
 
-//I used http://www.cplusplus.com/reference/vector/vector/ to reference the vector class and 
-
 Graph::Graph(unsigned int numNodes){
 	adjList.resize(numNodes);
 	for (int i = 0; i < numNodes; i++)	{
@@ -15,32 +13,29 @@ Graph::Graph(unsigned int numNodes){
 }
 
 int Graph::getCost(int node1, int node2){
-	return adjList[node1].edgeList[node2].cost;
+	Node node = adjList.at(node1);
+	Edge edge = node.edgeList[node2];
+	if (edge.dest = -1)							//oddly, this reminds me hashtables
+		return -1;
+	return edge.cost;
 	
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-	if (cost < 0) 
-		throw std::string("Cost cannot be less than 0");
+	Node* addTo = &adjList[node1];				//pointer to node containing edgelist we want
+	addTo->edgeList[node2].cost = cost;			//since the index of the edgelist is the destination, I don't think we need to update the destination?
+	
+	addTo = &adjList[node2];
+	addTo->edgeList[node1].cost = cost;
 
-	adjList[node2].edgeList.push_back(Edge(cost, node1));
-	adjList[node1].edgeList.push_back(Edge(cost, node2));
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-	for (int i = 0; i < adjList[node1].edgeList.size(); i++)	{
-		if (adjList[node1].edgeList[i].dest = node2)	{
-			adjList[node1].edgeList[i].cost = -1;
-			for (int i = 0; i < adjList[node2].edgeList.size(); i++)	{
-				if (adjList[node2].edgeList[i].dest = node1)	{
-					//adjList[node2].edgeList.erase(i);
-					//adjList[node2].edgeList[i].cost = -1;
-				}
-			}
-		}
-	}
+	Node* toRemove = &adjList[node1];
+	toRemove->edgeList[node2].cost = -1;
+	toRemove->edgeList[node2].dest = -1;
 }
